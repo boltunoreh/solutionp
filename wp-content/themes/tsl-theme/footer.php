36cc3d182ee808d2e8322b90ad5510e0996c0a29
boltun_oreh <alazankin@gmail.com>
2016-12-16 00:40:00 +0300

    change main template

--- wp-content/themes/tsl-theme/footer.php
+++ wp-content/themes/tsl-theme/footer.php
@@ -1,11 +1,2 @@
-
-	<?php wp_footer(); ?>
-	<script>
-		$(document).ready(function() {
-			<? $objPost = wp_count_posts('product'); 
-			$allCount = $objPost->publish; ?>
-		var catalog = new TSL_Catalog(<?=get_field('catalog_count','option'); ?>, <?=$allCount ?>, SimpleBasket);
-	});
-	</script>
 	</body>
 </html>
