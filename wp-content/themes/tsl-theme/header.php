f22280811bee2e75b970dd25577137b8ec97fdba
boltun_oreh <alazankin@gmail.com>
2016-12-18 23:59:17 +0300

    renew front

--- wp-content/themes/tsl-theme/header.php
+++ wp-content/themes/tsl-theme/header.php
@@ -1,81 +1,30 @@
 <!DOCTYPE html>
-<html lang="ru" class="fp-enabled" style="overflow: visible; height: initial;">
+<html lang="ru">
 <head>
-    <style type="text/css">.gm-style .gm-style-mtc label, .gm-style .gm-style-mtc div {
-            font-weight: 400
-        }</style>
-    <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
-    <style type="text/css">.gm-style .gm-style-cc span, .gm-style .gm-style-cc a, .gm-style .gm-style-mtc div {
-            font-size: 10px
-        }</style>
-    <style type="text/css">@media print {
-            .gm-style .gmnoprint, .gmnoprint {
-                display: none
-            }
-        }
-
-        @media screen {
-            .gm-style .gmnoscreen, .gmnoscreen {
-                display: none
-            }
-        }</style>
-    <style type="text/css">.gm-style {
-            font-family: Roboto, Arial, sans-serif;
-            font-size: 11px;
-            font-weight: 400;
-            text-decoration: none
-        }
-
-        .gm-style img {
-            max-width: none
-        }</style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1,maximum-scale=1, user-scalable=no"/>
     <title><? single_post_title(); ?></title>
-    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1,maximum-scale=1, user-scalable=no">
-    <meta name="title" content="Solution P">
-    <meta name="description" content="Solution P">
+    <meta name="title" content="Solution P"/>
+    <meta name="description" content="Solution P"/>
 
-    <link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/favicon.ico">
-    <link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/favicon-16x16.png" sizes="16x16">
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="./img/apple-touch-icon-152x152.png">
+    <link rel="canonical" href="http://solutionp.ru/" />
+    <link rel="shortlink" href="http://solutionp.ru/" />
+    <link rel="Shortcut Icon" type="image/x-icon" href="./img/favicon.ico"/>
+    <link rel="icon" type="image/png" href="./img/favicon-32x32.png" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="./img/favicon-16x16.png" sizes="16x16"/>
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="./img/apple-touch-icon-152x152.png"/>
 
-    <link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri(); ?>/style.css">
-    <link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri(); ?>/assets/css/jquery.fullPage.css">
-
+    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/jquery.fullPage.css"/>
+    <link rel='stylesheet' id='contact-form-7-css'  href="./plugins/contact-form-7/includes/css/styles.css"/>
     <!--[if IE 9]>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function(){
             $("body").addClass("ie-9");
         });
     </script>
     <![endif]-->
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps-api-v3/api/js/27/5/intl/ru_ALL/common.js"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps-api-v3/api/js/27/5/intl/ru_ALL/map.js"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps-api-v3/api/js/27/5/intl/ru_ALL/util.js"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps-api-v3/api/js/27/5/intl/ru_ALL/marker.js"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps-api-v3/api/js/27/5/intl/ru_ALL/onion.js"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps/api/js/ViewportInfoService.GetViewportInfo?1m6&amp;1m2&amp;1d55.713006799016156&amp;2d37.40467335584401&amp;2m2&amp;1d55.7718699328824&amp;2d37.604048486681336&amp;2u15&amp;4sru-RU&amp;5e0&amp;6sm%40369000000&amp;7b0&amp;8e0&amp;callback=_xdc_._bxqyd2&amp;token=5433"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps-api-v3/api/js/27/5/intl/ru_ALL/stats.js"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps-api-v3/api/js/27/5/intl/ru_ALL/controls.js"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps/vt?pb=!1m4!1m3!1i15!2i19794!3i10244!1m4!1m3!1i15!2i19794!3i10245!1m4!1m3!1i15!2i19795!3i10244!1m4!1m3!1i15!2i19795!3i10245!1m4!1m3!1i15!2i19794!3i10246!1m4!1m3!1i15!2i19794!3i10247!1m4!1m3!1i15!2i19795!3i10246!1m4!1m3!1i15!2i19795!3i10247!1m4!1m3!1i15!2i19796!3i10244!1m4!1m3!1i15!2i19796!3i10245!1m4!1m3!1i15!2i19797!3i10244!1m4!1m3!1i15!2i19797!3i10245!1m4!1m3!1i15!2i19796!3i10246!1m4!1m3!1i15!2i19796!3i10247!1m4!1m3!1i15!2i19797!3i10246!1m4!1m3!1i15!2i19797!3i10247!1m4!1m3!1i15!2i19798!3i10244!1m4!1m3!1i15!2i19798!3i10245!1m4!1m3!1i15!2i19799!3i10244!1m4!1m3!1i15!2i19799!3i10245!1m4!1m3!1i15!2i19798!3i10246!1m4!1m3!1i15!2i19798!3i10247!1m4!1m3!1i15!2i19799!3i10246!1m4!1m3!1i15!2i19799!3i10247!1m4!1m3!1i15!2i19800!3i10244!2m3!1e0!2sm!3i369048363!3m9!2sru-RU!3sUS!5e18!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e3!12m1!5b1&amp;callback=_xdc_._j4ntnl&amp;token=110771"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps/vt?pb=!1m4!1m3!1i15!2i19800!3i10245!1m4!1m3!1i15!2i19800!3i10246!1m4!1m3!1i15!2i19800!3i10247!2m3!1e0!2sm!3i369048363!3m9!2sru-RU!3sUS!5e18!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e3!12m1!5b1&amp;callback=_xdc_._sw4ndx&amp;token=51224"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps/api/js/AuthenticationService.Authenticate?1shttps%3A%2F%2Ffascfascfasc.github.io%2Fsolution-p%2F&amp;4sAIzaSyAQ0PT-P1EWEpIGfPeDHhgjfOWWe47I56g&amp;callback=_xdc_._19ure5&amp;token=33418"></script>
-    <script type="text/javascript" charset="UTF-8"
-            src="https://maps.googleapis.com/maps/api/js/QuotaService.RecordEvent?1shttps%3A%2F%2Ffascfascfasc.github.io%2Fsolution-p%2F&amp;3sAIzaSyAQ0PT-P1EWEpIGfPeDHhgjfOWWe47I56g&amp;5e0&amp;6u1&amp;7sqsioug&amp;callback=_xdc_._m3yikd&amp;token=23340"></script>
     <!-- WP_HEAD -->
     <?php wp_head() ?>
 </head>
-<body class="fp-viewing-intro fp-responsive" style="overflow: visible; height: initial;">
+<body>
