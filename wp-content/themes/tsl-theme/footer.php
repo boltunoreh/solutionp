70d5d370789ce9e99985bbdca26183887bb39306
Pavel Ivanov <pavel.ivanov@isobar.ru>
2016-12-27 13:26:48 +0300

    fix map back

--- wp-content/themes/tsl-theme/footer.php
+++ wp-content/themes/tsl-theme/footer.php
@@ -1,54 +1,32 @@
         <!-- Скрипты -->
         <?php $assets_dir = get_assets_dir(); ?>
         <script>
-            function initMap() {
-                var myLatLng = {
-                    <?php
-                    if (!$lat = get_field('index_map_lat')) {
-                        $lat = 55.750023;
-                    }
-                    ?>
-                    lat: <?= $lat; ?>,
-
-                    <?php
-                    if (!$lng = get_field('index_map_lng')) {
-                        $lng = 55.750023;
-                    }
-                    ?>
-                    lng: <?= $lng; ?>
-                };
-                // Create a map object and specify the DOM element for display
-                var map = new google.maps.Map(document.getElementById('map'), {
-                    center: myLatLng,
-                    scrollwheel: false,
-                    zoom: 15
-                });
-                // Create a bubble for marker
-                var infowindow = new google.maps.InfoWindow({
-                    <?php
-                    if (!$content = get_field('index_map_text')) {
-                        $content = '';
-                    }
-                    ?>
-                    content: '<span style="color:black;"><?= $content; ?></span>'
-                });
-                // Create a marker and set its position
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: myLatLng,
-                    icon: '<?= $assets_dir; ?>/img/map-marker.svg',
-                    <?php
-                    if (!$title = get_field('index_map_title')) {
-                        $title = '';
-                    }
-                    ?>
-                    title: '<?= $title; ?>'
-                });
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker);
-                });
+            <?php
+            if (!$lat = get_field('index_map_lat')) {
+                $lat = 55.750023;
             }
-            google.maps.event.addDomListener(window, 'load', initMap);
+            ?>
+            var gmlat = <?= $lat ?>;
+            <?php
+            if (!$lng = get_field('index_map_lng')) {
+                $lng = 55.750023;
+            }
+            ?>
+            var gmlng = <?= $lng ?>;
+
+            var gmicon = '<?= $assets_dir; ?>/img/map-marker.svg';
+            <?php
+            if (!$title = get_field('index_map_title')) {
+                $title = '';
+            }
+            ?>
+            var gmtitle = '<?= $title; ?>';
+            <?php
+            if (!$content = get_field('index_map_text')) {
+                $content = '';
+            }
+            ?>
+            var gmcontent = '<span style="color:black;"><?= $content; ?></span>';
         </script>
         <?php wp_footer(); ?>
     </body>
